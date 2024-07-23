2973f504a812cbc9e055fdf1f996efc18373569f
Andy Tran <anhquoctran006@gmail.com>
2024-07-22 21:35:49 -0600

    progressed on parser - still fighting the borrow checker

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -15,10 +15,10 @@ pub trait Expression: Node {
 
 // Holds an array of statements - implements Node which represents the ROOT node
 pub struct Program {
-    statements: Vec<Box<dyn Statement>>,
+    pub statements: Vec<Box<dyn Statement>>,
 }
 
-pub impl Node for Program {
+impl Node for Program {
     fn token_literal(&self) -> &str {
         if self.statements.len() > 0 {
             self.statements[0].token_literal()
@@ -31,15 +31,15 @@ pub impl Node for Program {
 
 // Identifier - implements an expression node
 pub struct Identifier {
-    token: Token,
-    value: String,
+    pub token: Token,
+    pub value: String,
 }
 
-pub impl Expression for Identifier {
+impl Expression for Identifier {
     fn expression_node(&self) {}
 }
 
-pub impl Node for Identifier {
+impl Node for Identifier {
     fn token_literal(&self) -> &str {
         &self.token.literal
     }
@@ -47,16 +47,16 @@ pub impl Node for Identifier {
 
 // Let Statement - implements Statement node
 pub struct LetStatement {
-    token: Token,
-    name: Box<Identifier>,
-    value: Box<dyn Expression>,
+    pub token: Token,
+    pub name: Box<Identifier>,
+    pub value: Box<dyn Expression>,
 }
 
-pub impl Statement for LetStatement {
+impl Statement for LetStatement {
     fn statement_node(&self) {}
 }
 
-pub impl Node for LetStatement {
+impl Node for LetStatement {
     fn token_literal(&self) -> &str {
         &self.token.literal
     }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,4 +2,34 @@ use crate::ast::*;
 use crate::lexer::*;
 use crate::token::*;
 
-pub struct Parser {}
+pub struct Parser {
+    l: Lexer,
+    cur_token: Option<Token>,
+    peek_token: Option<Token>,
+}
+
+impl Parser {
+    fn new(l: Lexer) -> Self {
+        let mut p = Self {
+            l: l,
+            cur_token: None,
+            peek_token: None,
+        };
+        // Read two tokens, so the two pointers get set
+        p.next_token();
+        p.next_token();
+        p
+    }
+
+    fn next_token(&mut self) {
+        self.cur_token = self.peek_token;
+        self.peek_token = Some(self.l.next_token());
+    }
+
+    fn parse_program() -> Option<Program> {
+        let program = Program {
+            statements: Vec::new(),
+        };
+        None
+    }
+}
