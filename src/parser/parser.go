26e4b793e4109bd38b9f217d2d99b14b974651d6
Andy Tran <anhquoctran006@gmail.com>
2024-08-22 11:28:04 -0600

    progressing on parser

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/andynapoleon/GoMonkeyLangInterpreter/ast"
 	"github.com/andynapoleon/GoMonkeyLangInterpreter/lexer"
 	"github.com/andynapoleon/GoMonkeyLangInterpreter/token"
@@ -11,15 +13,28 @@ type Parser struct {
 	l         *lexer.Lexer
 	curToken  token.Token
 	peekToken token.Token
+	errors    []string
 }
 
 // Create a new Parser structure
 func New(l *lexer.Lexer) *Parser { // takes in a lexer
-	p := &Parser{l: l} // curToken and peekToken are 0
+	p := &Parser{l: l, errors: []string{}} // curToken and peekToken are 0
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
 	return p
+}
+
+// Get all errors of the parser
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+// Add an error to errors when peekToken does not match expected type
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 // Move to next token in the lexer
@@ -33,6 +48,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 	program := &ast.Program{}
 	program.Statements = []ast.Statement{}
 
+	// Loop through all tokens and parse
 	for p.curToken.Type != token.EOF {
 		stmt := p.parseStatement()
 		if stmt != nil {
@@ -51,10 +67,51 @@ func (p *Parser) parseStatement() ast.Statement {
 	default:
 		return nil
 	}
-
 }
 
 // Parse a LET statement
 func (p *Parser) parseLetStatement() *ast.LetStatement {
-	return nil
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	// Is the next token an IDENT?
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	// Next token is indeed an identifier
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	// Is the next token an assign sign?
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: We're skipping the expressions until we
+	// encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+// Check type of current token
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+// Check type of next token
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+// Check type of current token
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
 }
